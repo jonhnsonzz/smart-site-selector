17a8216eb327a3ff532561527c5681c2b1c172c7
jonhnsonzz <56570974+jonhnsonzz@users.noreply.github.com>
2026-05-20 18:26:30 +0800

    feat: add sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'smart-site-selector-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      if (resp.ok) {
+        const clone = resp.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+      }
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
